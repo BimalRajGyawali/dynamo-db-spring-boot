6f331ffa9a280c6cacedbcffd676954924c86e01
Bimal Raj Gyawali <bimalraj.gyawali@codehimalaya.com>
2022-09-29 16:49:36 +0545

    refactor: moves properties to file

--- src/main/kotlin/com/personal/dynamospringboot/config/DynamoDBProperties.kt
+++ src/main/kotlin/com/personal/dynamospringboot/config/DynamoDBProperties.kt
@@ -1,13 +1,23 @@
 package com.personal.dynamospringboot.config
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Component
 
 @Component
+@PropertySource("classpath:dynamodb.properties")
 class DynamoDBProperties {
 
-    val serviceEndpoint = "http://localhost:8000"
-    val region = "ap-south-1"
-    val accessKey = "local"
-    val secretKey = "local"
+    @Value("\${aws.dynamodb.service-endpoint}")
+    lateinit var serviceEndpoint: String
+
+    @Value("\${aws.dynamodb.region}")
+    lateinit var region: String
+
+    @Value("\${aws.dynamodb.access-key}")
+    lateinit var accessKey: String
+
+    @Value("\${aws.dynamodb.secret-key}")
+    lateinit var secretKey: String
 
 }
